bd00b144b0dcc686276a5be25646b57ad3d0aa7b
Dewa77661 <101981545+Dewa77661@users.noreply.github.com>
2025-02-09 09:27:37 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,13 @@ import { getFirestore } from "firebase/firestore"
 import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmUIXJblRGF4HTCO3hOB8MykvLOq7whUE",
-  authDomain: "project-web-kelas.firebaseapp.com",
-  projectId: "project-web-kelas",
-  storageBucket: "project-web-kelas.appspot.com",
-  messagingSenderId: "202691312352", 
-  appId: "1:202691312352:web:e82e7a494e705b2a12ee85"
+  apiKey: "AIzaSyCh9DNBC653gh1QcXCEpAjzX40ynbNYm7A",
+  authDomain: "bpride-d0177.firebaseapp.com",
+  projectId: "bpride-d0177",
+  storageBucket: "bpride-d0177.firebasestorage.app",
+  messagingSenderId: "348157981252",
+  appId: "1:348157981252:web:51b1c5b27268196dda0211",
+  measurementId: "G-9PJG4CXWLN"
 
 };
 
